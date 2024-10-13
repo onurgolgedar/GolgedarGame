8f284ca673b423ba66702496add14d707059d649
onurgolgedar <b.onur.golgedar@gmail.com>
2024-10-13 14:12:41 -0700

    TCP Connection Implementation

--- 1- Objects/Client.cs
+++ 1- Objects/Client.cs
@@ -1,4 +1,8 @@
-﻿using SFML.Window;
+﻿using GolgedarEngine;
+using SFML.Window;
+using System;
+using System.Collections.Generic;
+using System.Text.Json;
 
 namespace GolgedarClient
 {
@@ -20,6 +24,71 @@ namespace GolgedarClient
 
             if (Game.IsKeyPressed(Keyboard.Key.Q))
                 Send('Q');
+
+            if (Game.IsKeyPressed(Keyboard.Key.Space))
+                Send<List<string>>('T', ["", "2", "3"]);
+        }
+
+        public override void Process(int code, float data)
+        {
+            switch (code)
+            {
+                case 'P':
+                    Console.WriteLine(data);
+                    break;
+            }
+        }
+
+        public override void Process(int code, string data, string typeName)
+        {
+            switch (code)
+            {
+                case 'T':
+                    Console.WriteLine(data);
+                    Console.WriteLine(typeName);
+                    Console.WriteLine();
+                    break;
+            }
+        }
+
+        public override void Process(int code, string data)
+        {
+            switch (code)
+            {
+                case 'P':
+                    Console.WriteLine(data);
+                    break;
+            }
+        }
+
+        public override void Process(int code, double data)
+        {
+            switch (code)
+            {
+                case 'P':
+                    Console.WriteLine(data);
+                    break;
+            }
+        }
+
+        public override void Process(int code, char data)
+        {
+            switch (code)
+            {
+                case 'P':
+                    Console.WriteLine(data);
+                    break;
+            }
+        }
+
+        public override void Process(int code, int data)
+        {
+            switch (code)
+            {
+                case 'P':
+                    Console.WriteLine(data);
+                    break;
+            }
         }
     }
 }
