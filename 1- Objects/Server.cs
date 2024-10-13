84a10a0813f82e7331a0bb7d488f2c74286b84a3
onurgolgedar <b.onur.golgedar@gmail.com>
2024-10-13 14:11:56 -0700

    TCP Connection Implementation

--- 1- Objects/Server.cs
+++ 1- Objects/Server.cs
@@ -1,4 +1,6 @@
 ﻿using GolgedarEngine;
+using SFML.Window;
+using System.Collections.Generic;
 using System.Linq;
 using Vector = GolgedarEngine.Vector;
 
@@ -6,8 +8,42 @@ namespace GolgedarServer
 {
     internal class Server : GolgedarEngine.Server
     {
+        public override void Loop()
+        {
+            if (Game.CheckKey(Keyboard.Key.W))
+            {
+                Users.ForEach(user =>
+                {
+                    Player player = user.ConnectedObjects.FirstOrDefault() as Player;
+                    if (player != null)
+                    {
+                        Send(user, 'P', player.Position.X);
+                        Send(user, 'P', 231f);
+                        Send(user, 'P', 231.8);
+                        Send(user, 'P', '2');
+                        Send(user, 'P', "selam");
+                    }
+                });
+            }
+
+            if (Game.CheckKey(Keyboard.Key.T))
+            {
+                Users.ForEach(user =>
+                {
+                    Send(user, 'T', new Obstacle());
+                });
+            }
+        }
+
+        public override void Process(User user, int code, string data, string typeName)
+        {
+            base.Process(user, code, data, typeName);
+        }
+
         public override void Process(User user, int code, char data)
         {
+            base.Process(user, code, data);
+
             Player player;
             switch (code)
             {
